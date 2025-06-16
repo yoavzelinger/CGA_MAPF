422d1faa4f45edafbfe90ba17e02719075355800
Yoav Zelinger <yoavzelinger11@gmail.com>
2025-06-16 20:38:36 +0300

    update main run

--- algs/alg_mapf_pibt.py
+++ algs/alg_mapf_pibt.py
@@ -1,3 +1,6 @@
+from argparse import ArgumentParser
+import csv
+
 from algs.alg_functions_pibt import *
 from run_single_MAPF_func import run_mapf_alg
 
@@ -72,14 +75,58 @@ def run_pibt(
     runtime = time.time() - start_time
     return {a.name: a.path for a in agents}, {'agents': agents, 'time': runtime, 'makespan': iteration}
 
+parser = ArgumentParser(description="Run all tests")
+parser.add_argument("-e", "--environment", type=str, help="The environment - map name", required=True)
+parser.add_argument("-s", "--scenario_index", type=int, help="Scenario index", default=-1)
+parser.add_argument("-p", "--plot", type=str, help="Plot the results of <scenario_index total_agents inactive_agents>", default="")
+args = parser.parse_args()
 
-@use_profiler(save_dir='../stats/alg_pibt.pstat')
+map_name = args.environment
+active_agents_amounts = [25, 50, 75, 100, 125, 150]
+inactive_agents_amounts = [0, 25, 50, 75, 100, 125, 150]
+
+if not os.path.exists("Output_files"):
+    os.makedirs("Output_files")
+
+with open(f"Output_files/Output_{map_name}_PIBT.csv", mode="w", newline="",
+          encoding="utf-8") as file:
+    columns = ["map_name", "scenario_index", "total_agents", "active_agents", "inactive_agents", "density", "SOC", "makespan", "runtime"]
+    writer = csv.DictWriter(file, fieldnames=columns)
+    writer.writeheader()
+
+def addRecordToCsv(current_values):
+
+    record = list(current_values)
+
+    with open(f"Output_files/Output_{map_name}_PIBT.csv", mode="a", newline="", encoding="utf-8") as file:
+        writerRecord = csv.writer(file)
+        writerRecord.writerow(record)
+
+@use_profiler(save_dir='./stats/alg_pibt.pstat')
 def main():
-    params = {
-        'max_time': 100,
-    }
-    run_mapf_alg(alg=run_pibt, params=params)
 
+    to_render = False
+    # to_render = False
+
+    params = {
+        'max_time': 30,
+        'alg_name': 'PIBT',
+        'alt_goal_flag': 'first',
+        # 'alt_goal_flag': 'num', 'alt_goal_num': 3,
+        # 'alt_goal_flag': 'all',
+        'to_render': to_render,
+    }
+    if args.plot:
+        scenario_index, active_agents, inactive_agents = tuple(map(int, args.plot.split()))
+        run_mapf_alg(alg=run_pibt, params=params, final_render=True, map_name=map_name, active_agents=active_agents, inactive_agents=inactive_agents, scenario_index=scenario_index)
+        return
+
+    for active_agents in active_agents_amounts:
+        for inactive_agents in inactive_agents_amounts:
+            print(f'\nRunning with {active_agents=} and {inactive_agents=}')
+            for scenario_index in range(1, 26):
+                current_values = run_mapf_alg(alg=run_pibt, params=params, final_render=False, map_name=map_name, active_agents=active_agents, inactive_agents=inactive_agents, scenario_index=scenario_index)
+                addRecordToCsv(current_values)
 
 if __name__ == '__main__':
     main()
